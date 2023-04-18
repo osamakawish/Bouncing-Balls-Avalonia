c507fd5edfd177eb3de717c93cfc1614a66451f3
Osama Kawish <osamakawish@gmail.com>
2023-04-18 13:55:28 -0400

    Implemented basic ball animation.

--- Bouncing Balls WPF/Views/MainWindow.xaml.cs
+++ Bouncing Balls WPF/Views/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Bouncing_Balls.ViewModels;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace Bouncing_Balls_WPF.Views;
 
@@ -13,6 +15,8 @@ public partial class MainWindow
 {
     private MainWindowViewModel? _dataContext;
 
+    private Storyboard Storyboard { get; } = new();
+
     public MainWindow()
     {
         InitializeComponent();
@@ -20,6 +24,22 @@ public partial class MainWindow
         Loaded += delegate {
             _dataContext = (MainWindowViewModel)DataContext!;
             AddBall();
+
+            Storyboard.Begin();
+        };
+
+        Canvas.SizeChanged += (_, args) =>
+        {
+            var w = args.NewSize.Width / args.PreviousSize.Width;
+            var h = args.NewSize.Height / args.PreviousSize.Height;
+
+            foreach (var obj in Canvas.Children)
+            {
+                if (obj is not UIElement item) continue;
+
+                Canvas.SetLeft(item, Canvas.GetLeft(item) * w);
+                Canvas.SetTop(item, Canvas.GetTop(item) * h);
+            }
         };
     }
 
@@ -32,16 +52,20 @@ public partial class MainWindow
             Fill = Brushes.Red
         };
 
-        Canvas.SetLeft(ball, 0.5 * Canvas.ActualWidth); Canvas.SetTop(ball, 0.333 * Canvas.ActualHeight);
+        Canvas.SetLeft(ball, 0.5 * Canvas.ActualWidth);
+        Canvas.SetTop(ball, 0.5 * Canvas.ActualHeight);
+
         Canvas.Children.Add(ball);
 
-        Canvas.SizeChanged += (_, args) =>
+        var animation = new DoubleAnimation
         {
-            var w = args.NewSize.Width  / args.PreviousSize.Width ;
-            var h = args.NewSize.Height / args.PreviousSize.Height;
-            
-            Canvas.SetLeft(ball, Canvas.GetLeft(ball) * w);
-            Canvas.SetTop (ball, Canvas.GetTop (ball) * h);
+            From = 0.5 * Canvas.ActualHeight,
+            To = 0.8 * Canvas.ActualHeight,
+            Duration = new (System.TimeSpan.FromSeconds(5))
         };
+
+        Storyboard.SetTarget(animation, ball);
+        Storyboard.SetTargetProperty(animation, new(Canvas.TopProperty));
+        Storyboard.Children.Add(animation);
     }
 }
